d8151ee48791db82f0c4e2e1cf4584a180b63475
supto707 <kaunia.supto@gmail.com>
2026-01-04 03:53:23 +0600

    server fixing 5

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,7 +23,6 @@ app.use(cors({
 app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
-app.use(morgan('dev'));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
@@ -41,8 +39,6 @@ const paymentRoutes = require('./routes/payments');
 const notificationRoutes = require('./routes/notifications');
 const statsRoutes = require('./routes/stats');
 const reportRoutes = require('./routes/reports');
-
-const errorHandler = require('./middleware/error');
 
 // Mount Routes
 app.use('/api/auth', authRoutes);
@@ -61,7 +57,10 @@ app.get('/', (req, res) => {
 });
 
 // Error Handler
-app.use(errorHandler);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
